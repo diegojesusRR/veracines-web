a2864d3ca000cf92d1559886d2a1c93754ba913a
djramosMac <djramos@meteored.com>
2024-04-06 18:51:02 +0200

    no message

---
+++ src/consts/metadata.ts
@@ -0,0 +1,24 @@
+export const Metadata: Record<string, { title: string, description: string, canonical: string }> = {
+    home: {
+        title: "Veracines - Cines de Verano en Vera y Garrucha",
+        description: "Cine de verano en Vera y Garrucha. Descubre las películas de estreno en Vera y Garrucha. Disvruta la experiencia del cine de verano en Veracines.",
+        canonical: "www.veracines.es"
+    },
+
+    "cartelera-vera": {
+        title: "Cartelera Veracines",
+        description: "Cartelera del cine de verano en Vera. Descubre las películas de estreno en Vera. Disvruta la experiencia del cine de verano en Veracines.",
+        canonical: "www.veracines.es/cartelera-vera"
+    },
+    "cartelera-garrucha": {
+        title: "Cartelera Cines",
+        description: "Cartelera de Cine Tenis de Garrucha. Descubre las películas de estreno en Garrucha. Disvruta la experiencia del cine de verano en Veracines.",
+        canonical: "www.veracines.es/cartelera-cines"
+    },
+    "cartelera-regio": {
+        title: "Cartelera Regio",
+        description: "Cartelera del cine de invierno en Vera. Descubre las películas de estreno en Vera. Disvruta la experiencia del cine Regio en Veracines.",
+        canonical: "www.veracines.es/cartelera-regio"
+    },
+
+} as const
