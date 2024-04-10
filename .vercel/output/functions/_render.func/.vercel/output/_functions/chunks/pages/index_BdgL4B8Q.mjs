7097bd4d52781185f185dc52ce377f8382ec9b68
djramosMac <djramos@meteored.com>
2024-04-10 15:34:36 +0200

    no message

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/pages/index_BdgL4B8Q.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/pages/index_BdgL4B8Q.mjs
@@ -9,9 +9,9 @@ import { C as COMBATS, R as REY_DE_LA_PISTA_ID } from './get-forecast-by-combat-
 import 'clsx';
 /* empty css                          */
 
-const $$Astro$3 = createAstro("https://www.veracines.es");
+const $$Astro$5 = createAstro("https://www.veracines.es");
 const $$Index$1 = createComponent(async ($$result, $$props, $$slots) => {
-  const Astro2 = $$result.createAstro($$Astro$3, $$props, $$slots);
+  const Astro2 = $$result.createAstro($$Astro$5, $$props, $$slots);
   Astro2.self = $$Index$1;
   const getCombatById = (id) => {
     return COMBATS.find((combat) => combat.id === id);
@@ -49,9 +49,9 @@ const index$1 = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
 	url: $$url$1
 }, Symbol.toStringTag, { value: 'Module' }));
 
-const $$Astro$2 = createAstro("https://www.veracines.es");
+const $$Astro$4 = createAstro("https://www.veracines.es");
 const $$Hero = createComponent(async ($$result, $$props, $$slots) => {
-  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  const Astro2 = $$result.createAstro($$Astro$4, $$props, $$slots);
   Astro2.self = $$Hero;
   return renderTemplate`${maybeRenderHead()}<section class="flex flex-col place-items-center gap-4 lg:gap-9" data-astro-cid-shwxrd6l> <h1 class="sr-only bg-black/30" data-astro-cid-shwxrd6l>Veracines</h1> </section> `;
 }, "/Users/diego.ramos/veracines-web/src/sections/Hero.astro", void 0);
@@ -79,21 +79,35 @@ const Metadata = {
   }
 };
 
-const $$Astro$1 = createAstro("https://www.veracines.es");
+const $$Astro$3 = createAstro("https://www.veracines.es");
 const $$PeliculaPrincipal = createComponent(async ($$result, $$props, $$slots) => {
-  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  const Astro2 = $$result.createAstro($$Astro$3, $$props, $$slots);
   Astro2.self = $$PeliculaPrincipal;
-  return renderTemplate`${maybeRenderHead()}<div> ${renderComponent($$result, "Action", $$Action, { "class": "w-full text-base", "as": "a", "aria-disabled": "false", "href": "https://kinetike.com:83/views/init.aspx?cine=REGIO", "target": "_blank", "rel": "noopener noreferrer", "aria-label": "Enlace a la venta de entradas" }, { "default": ($$result2) => renderTemplate`
-Comprar entradas
-` })} <img src="/img/pelicula-principal.jpeg"> </div>`;
+  return renderTemplate`${maybeRenderHead()}<div> <img src="/img/pelicula-principal.jpeg"> </div>`;
 }, "/Users/diego.ramos/veracines-web/src/components/peliculaPrincipal.astro", void 0);
+
+const $$Astro$2 = createAstro("https://www.veracines.es");
+const $$ComprarEntradas = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  Astro2.self = $$ComprarEntradas;
+  return renderTemplate`${renderComponent($$result, "Action", $$Action, { "class": "w-full text-base", "as": "a", "aria-disabled": "false", "href": "https://kinetike.com:83/views/init.aspx?cine=REGIO", "target": "_blank", "rel": "noopener noreferrer", "aria-label": "Enlace a la venta de entradas" }, { "default": ($$result2) => renderTemplate`
+Comprar entradas
+` })}`;
+}, "/Users/diego.ramos/veracines-web/src/components/ComprarEntradas.astro", void 0);
+
+const $$Astro$1 = createAstro("https://www.veracines.es");
+const $$SiguientePelicula = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$SiguientePelicula;
+  return renderTemplate`${maybeRenderHead()}<div> <img src="/img/siguiente-pelicula.jpeg"> </div>`;
+}, "/Users/diego.ramos/veracines-web/src/components/siguientePelicula.astro", void 0);
 
 const $$Astro = createAstro("https://www.veracines.es");
 const prerender = false;
 const $$Index = createComponent(async ($$result, $$props, $$slots) => {
   const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
   Astro2.self = $$Index;
-  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "description": Metadata.home.description, "title": Metadata.home.title, "canonical": Metadata.home.canonical }, { "default": ($$result2) => renderTemplate` ${renderComponent($$result2, "Hero", $$Hero, {})} ${maybeRenderHead()}<main> ${renderComponent($$result2, "PeliculaPrincipal", $$PeliculaPrincipal, {})} </main> ` })}`;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "description": Metadata.home.description, "title": Metadata.home.title, "canonical": Metadata.home.canonical }, { "default": ($$result2) => renderTemplate` ${renderComponent($$result2, "Hero", $$Hero, {})} ${maybeRenderHead()}<main> ${renderComponent($$result2, "ComprarEntradas", $$ComprarEntradas, {})} ${renderComponent($$result2, "PeliculaPrincipal", $$PeliculaPrincipal, {})} ${renderComponent($$result2, "SiguientePelicula", $$SiguientePelicula, {})} </main> ` })}`;
 }, "/Users/diego.ramos/veracines-web/src/pages/index.astro", void 0);
 
 const $$file = "/Users/diego.ramos/veracines-web/src/pages/index.astro";
