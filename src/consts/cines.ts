c6c2250a9a94729e6bc4206d43fac22174be079b
djramosMac <djramos@meteored.com>
2024-05-18 19:11:34 +0200

    no message

--- src/consts/cines.ts
+++ src/consts/cines.ts
@@ -29,10 +29,11 @@ export const CINES: Cine[] = [
         nombre: "Cine Regio de Vera",
         color: "primary",
         localizacion: {
-            ubicacionKey: "!1m18!1m12!1m3!1d6352.183416503645!2d-1.8651944235623443!3d37.24553154246919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7ad505dc0deb95%3A0x20c4e16b3ec9e8ba!2sCine%20de%20Vera!5e0!3m2!1ses!2ses!4v1715622722058!5m2!1ses!2ses",
-            direccion: "C. Antonio de Torres, 2, 04620 Vera, Almería",
-            descripcion: "Situado junto al colegio Reyes Católicos, en la calle Antonio de Torres, el cine de verano de Vera es un lugar ideal para disfrutar de una película al aire libre en Vera."
+            ubicacionKey: "!1m18!1m12!1m3!1d3176.0496114881794!2d-1.8706054238921004!3d37.24653037212324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7ad503b6424b21%3A0xb294666e35a7e06c!2sC.%20Radio%20Nacional%2C%20s%2Fn%2C%2004620%20Vera%2C%20Almer%C3%ADa!5e0!3m2!1ses!2ses!4v1716051070703!5m2!1ses!2ses",
+            direccion: "C. Radio Nacional, s/n, 04620 Vera, Almería",
+            descripcion: "Situado en pleno centro de Vera, junto a la Plaza Mayor, en Cine Regio de Vera entre otorño y primavera podrás disfrutar de las mejores películas en un ambiente único."
         },
         servicios:[]
     },
     ];
+
