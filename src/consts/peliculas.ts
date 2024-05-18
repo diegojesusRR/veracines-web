c53cae3780cb19432499d50aa5a346486bb9e0c2
djramosMac <djramos@meteored.com>
2024-05-19 00:16:06 +0200

    no message

--- src/consts/peliculas.ts
+++ src/consts/peliculas.ts
@@ -14,14 +14,14 @@ export const PELICULAS: Pelicula[] = [
         id: "deadpool-y-lobezno",
         nombre: "Deadpool y Lobezno",
         descripcion: "Tercera entrega de la saga \"Deadpool\", ahora integrada en el Universo Cinematográfico de Marvel (MCU) pero manteniendo su enfoque para adultos, con calificación R. En septiembre de 2022 se confirmó la aparición de Hugh Jackman como Lobezno, por primera vez desde \"Logan\".",
-        edadRedomendada: "Mayores de 16 años",
+        edadRedomendada: "No recomendada menores 16 años",
         videoUrl: "https://www.youtube.com/embed/tTM5weeCFvQ"
     },
     {
         id: "bad-boys-ride-or-die",
         nombre: "Bad Boys: Ride or Die",
         descripcion: "Los policías más famosos del mundo regresan con su icónica mezcla de acción al límite y comedia escandalosa, pero esta vez con un giro inesperado: ¡Los mejores de Miami se dan a la fuga! Cuarta entrega de la saga 'Dos policías rebeldes'.",
-        edadRedomendada: "Mayores de 16 años",
+        edadRedomendada: "No recomendada menores 16 años",
         videoUrl: "https://www.youtube.com/embed/LOZqqEXURzg"
     }
 
