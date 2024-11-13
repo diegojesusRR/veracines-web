aeeb09138007f8fec49fb59cbd6fb21ad0d6c33c
djramosMac <djramos@meteored.com>
2024-11-13 11:19:50 +0100

    no message

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_D53ZYWb7.mjs';
-import { manifest } from './manifest_CHa1ozVf.mjs';
+import { manifest } from './manifest_DR9QxYNC.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/api/auth/_---auth_.astro.mjs');
@@ -31,7 +31,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "f027c815-9a18-4010-bfce-278e4f71f1bc"
+    "middlewareSecret": "a6eafc91-6253-4cff-93f9-8ecc12b86f54"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
