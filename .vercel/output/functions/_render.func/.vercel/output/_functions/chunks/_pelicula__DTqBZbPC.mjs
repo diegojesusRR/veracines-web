8a324c6b0922140af62c5cbf76775f43be8c731b
djramosMac <djramos@meteored.com>
2024-11-13 11:00:35 +0100

    no message

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_pelicula__DTqBZbPC.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_pelicula__iot_llD_.mjs');
-
-export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/metadata_BAvqYMtC.mjs
@@ -0,0 +1,24 @@
+const Metadata = {
+  home: {
+    title: "Cines de Verano en Vera y Garrucha - Veracines",
+    description: "Cine de verano en Vera y Garrucha. Descubre las películas de estreno en Vera y Garrucha. Disvruta la experiencia del cine de verano en Veracines.",
+    canonical: "https://www.veracines.es"
+  },
+  "cartelera-vera": {
+    title: "Cartelera Cine Terraza de Verano de Vera - Veracines",
+    description: "Cartelera del cine de verano en Vera. Descubre las películas de estreno en Vera. Disvruta la experiencia del cine de verano en Veracines.",
+    canonical: "https://www.veracines.es/cartelera-vera/"
+  },
+  "cartelera-garrucha": {
+    title: "Cartelera Cine Tenis de Garrucha - Veracines",
+    description: "Cartelera de Cine Tenis de Garrucha. Descubre las películas de estreno en Garrucha. Disvruta la experiencia del cine de verano en Veracines.",
+    canonical: "https://www.veracines.es/cartelera-garrucha/"
+  },
+  "cartelera-regio": {
+    title: "Cartelera Cine Regio de Vera - Veracines",
+    description: "Cartelera del cine Regio de invierno en Vera. Descubre las películas de estreno en Vera. Disvruta la experiencia del cine Regio en Veracines.",
+    canonical: "https://www.veracines.es/cartelera-regio/"
+  }
+};
+
+export { Metadata as M };
