bfbb6563892c2eb980b402ab94ffb1083c9a5ee3
djramosMac <djramos@meteored.com>
2024-11-14 18:58:34 +0100

    no message

--- .netlify/build/chunks/metadata_BZOBfiP0.mjs
+++ .netlify/build/chunks/metadata_BZOBfiP0.mjs
@@ -18,4 +18,27 @@ const $$Action = createComponent(($$result, $$props, $$slots) => {
   ], ...props }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<span${addAttribute(`inline-block skew-x-[21deg] text-xs`, "class")}>${renderSlot($$result2, $$slots["default"])}</span> ` })}`;
 }, "/Users/diego.ramos/veracines-web/src/components/Action.astro", void 0);
 
-export { $$Action as $ };
+const Metadata = {
+  home: {
+    title: "Cines de Verano en Vera y Garrucha - Veracines",
+    description: "Cine de verano en Vera y Garrucha. Descubre las películas de estreno en Vera y Garrucha. Disvruta la experiencia del cine de verano en Veracines.",
+    canonical: "https://www.veracines.es"
+  },
+  "cartelera-vera": {
+    title: "Cartelera Cine Terraza de Verano de Vera - Veracines",
+    description: "Cartelera del cine de verano en Vera. Descubre las películas de estreno en Vera. Disvruta la experiencia del cine de verano en Veracines.",
+    canonical: "https://www.veracines.es/cartelera-vera/"
+  },
+  "cartelera-garrucha": {
+    title: "Cartelera Cine Tenis de Garrucha - Veracines",
+    description: "Cartelera de Cine Tenis de Garrucha. Descubre las películas de estreno en Garrucha. Disvruta la experiencia del cine de verano en Veracines.",
+    canonical: "https://www.veracines.es/cartelera-garrucha/"
+  },
+  "cartelera-regio": {
+    title: "Cartelera Cine Regio de Vera - Veracines",
+    description: "Cartelera del cine Regio de invierno en Vera. Descubre las películas de estreno en Vera. Disvruta la experiencia del cine Regio en Veracines.",
+    canonical: "https://www.veracines.es/cartelera-regio/"
+  }
+};
+
+export { $$Action as $, Metadata as M };
