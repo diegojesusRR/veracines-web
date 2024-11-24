c7f08b46dff375be3d5972eded5ff5dc81b26333
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-11-24 20:31:05 +0100

    nuevo hotfix de adrian

---
+++ .netlify/build/pages/api/auth/_---auth_.astro.mjs
@@ -0,0 +1,82 @@
+import { Auth } from '@auth/core';
+import { parseString } from 'set-cookie-parser';
+import Twitch from '@auth/core/providers/twitch';
+export { renderers } from '../../../renderers.mjs';
+
+const defineConfig = (config) => {
+  config.prefix ??= "/api/auth";
+  return config;
+};
+
+const authConfig = defineConfig({
+  providers: [
+    Twitch({
+      clientId: undefined                                ,
+      clientSecret: undefined                                    
+    })
+  ],
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub
+      }
+    })
+  }
+});
+
+const __vite_import_meta_env__ = {"ASSETS_PREFIX": undefined, "BASE_URL": "/", "DEV": false, "MODE": "production", "PROD": true, "SITE": "https://www.veracines.es", "SSR": true};
+const actions = [
+  "providers",
+  "session",
+  "csrf",
+  "signin",
+  "signout",
+  "callback",
+  "verify-request",
+  "error"
+];
+function AstroAuthHandler(prefix, options = authConfig) {
+  return async ({ cookies, request }) => {
+    const url = new URL(request.url);
+    const action = url.pathname.slice(prefix.length + 1).split("/")[0];
+    if (!actions.includes(action) || !url.pathname.startsWith(prefix + "/")) return;
+    const res = await Auth(request, options);
+    if (["callback", "signin", "signout"].includes(action)) {
+      res.headers.getSetCookie().forEach((cookie) => {
+        const { name, value, ...options2 } = parseString(cookie);
+        cookies.set(name, value, options2);
+      });
+      res.headers.delete("Set-Cookie");
+    }
+    return res;
+  };
+}
+function AstroAuth(options = authConfig) {
+  const { AUTH_SECRET, AUTH_TRUST_HOST, VERCEL, NODE_ENV } = Object.assign(__vite_import_meta_env__, { _: process.env._, NODE: process.env.NODE, NODE_ENV: process.env.NODE_ENV });
+  options.secret ??= AUTH_SECRET;
+  options.trustHost ??= !!(AUTH_TRUST_HOST ?? VERCEL ?? NODE_ENV !== "production");
+  const { prefix = "/api/auth", ...authOptions } = options;
+  const handler = AstroAuthHandler(prefix, authOptions);
+  return {
+    async GET(context) {
+      return await handler(context);
+    },
+    async POST(context) {
+      return await handler(context);
+    }
+  };
+}
+
+const { GET, POST } = AstroAuth();
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	GET,
+	POST
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
