3976db27dd8abc83e5aa9551f4cef7c66c1b684b
Diego Jesús Ramos Rodríguez <>
2025-06-28 20:52:23 +0200

    no message

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_DLuvGk3V.mjs';
+import { manifest } from './manifest_CSdPqBiK.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -33,7 +33,7 @@ const _manifest = Object.assign(manifest, {
     middleware: undefined
 });
 const _args = {
-    "middlewareSecret": "bd10a0d5-6e5c-46d5-a8b1-bcf734321faa"
+    "middlewareSecret": "4019a15e-952f-431c-a3f0-56ff6c462071"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
