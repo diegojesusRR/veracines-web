ae7d39466853933c77f90198530ae929fc5a07b0
Diego Jesús Ramos Rodríguez <>
2025-07-21 15:50:16 +0200

    no message

--- src/consts/proyecciones.ts
+++ src/consts/proyecciones.ts
@@ -90,6 +90,38 @@ const gruposProyeccionesVera: GrupoProyeccion[] = [
             ...getProyecciones(new Date('2025-07-25'), new Date('2025-07-28'),  '22:00'),
         ],
     },
+    {
+        grupo: 9,
+        cine: cineVera,
+        pelicula: PELICULAS.find(pelicula => pelicula.id === 'f1-la-pelicula')!,
+        proyecciones: [
+            ...getProyecciones(new Date('2025-07-29'), new Date('2025-07-31'),  '22:00'),
+        ],
+    },
+    {
+        grupo: 10,
+        cine: cineVera,
+        pelicula: PELICULAS.find(pelicula => pelicula.id === 'los-tipos-malos-2')!,
+        proyecciones: [
+            ...getProyecciones(new Date('2025-08-01'), new Date('2025-08-04'),  '22:00'),
+        ],
+    },
+    {
+        grupo: 11,
+        cine: cineVera,
+        pelicula: PELICULAS.find(pelicula => pelicula.id === 'jurassic-world-el-renacer')!,
+        proyecciones: [
+            ...getProyecciones(new Date('2025-08-05'), new Date('2025-08-07'),  '22:00'),
+        ],
+    },
+    {
+        grupo: 12,
+        cine: cineVera,
+        pelicula: PELICULAS.find(pelicula => pelicula.id === 'karate-kid-leyendas')!,
+        proyecciones: [
+            ...getProyecciones(new Date('2025-08-08'), new Date('2025-08-11'),  '22:00'),
+        ],
+    },
 ]
 
 const cineRegio = CINES.find(cine => cine.url === 'regio')!;
@@ -154,6 +186,22 @@ const gruposProyeccionesRegio: GrupoProyeccion[] = [
         proyecciones: [
             ...getProyecciones(new Date('2025-07-22'), new Date('2025-07-24'),  '20:00'),
             ...getProyecciones(new Date('2025-07-22'), new Date('2025-07-24'),  '22:00'),
+        ],
+    },
+    {
+        grupo: 8,
+        cine: cineRegio,
+        pelicula: PELICULAS.find(pelicula => pelicula.id === 'voy-a-pasarmelo-mejor')!,
+        proyecciones: [
+            ...getProyecciones(new Date('2025-07-25'), new Date('2025-07-28'),  '21:00'),
+        ],
+    },
+    {
+        grupo: 9,
+        cine: cineRegio,
+        pelicula: PELICULAS.find(pelicula => pelicula.id === 'los-4-fantasticos-primeros-pasos')!,
+        proyecciones: [
+            ...getProyecciones(new Date('2025-07-29'), new Date('2025-07-31'),  '21:00'),
         ],
     },
 ]
